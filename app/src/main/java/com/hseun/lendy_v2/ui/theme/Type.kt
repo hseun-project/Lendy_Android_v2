4b5d268ef2bef5bd96544dab1b4c55274f70e7b4
hseun <hseun0732@dms.hs.kr>
2025-06-17 14:46:42 +0900

    add :: font

--- app/src/main/java/com/hseun/lendy_v2/ui/theme/Type.kt
+++ app/src/main/java/com/hseun/lendy_v2/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.hseun.lendy_v2.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hseun.lendy_v2.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +17,171 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+val Pretendard = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.pretendard_regular,
+            weight = FontWeight.Normal
+        ),
+        Font(
+            resId = R.font.pretendard_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.pretendard_semibold,
+            weight = FontWeight.SemiBold
+        ),
+        Font(
+            resId = R.font.pretendard_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
+
+object LendyFontStyle {
+    private val defaultTextColor = Black
+
+    val regular12 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        color = defaultTextColor
+    )
+
+    val medium10 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp,
+        color = defaultTextColor
+    )
+    val medium12 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        color = defaultTextColor
+    )
+    val medium13 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
+        color = defaultTextColor
+    )
+    val medium14 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        color = defaultTextColor
+    )
+    val medium15 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        color = defaultTextColor
+    )
+    val medium16 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        color = defaultTextColor
+    )
+    val medium17 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        color = defaultTextColor
+    )
+    val medium18 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        color = defaultTextColor
+    )
+
+    val semi11 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 11.sp,
+        color = defaultTextColor
+    )
+    val semi12 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        color = defaultTextColor
+    )
+    val semi13 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 13.sp,
+        color = defaultTextColor
+    )
+    val semi14 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        color = defaultTextColor
+    )
+    val semi15 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 15.sp,
+        color = defaultTextColor
+    )
+    val semi16 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        color = defaultTextColor
+    )
+    val semi18 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        color = defaultTextColor
+    )
+    val semi20 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        color = defaultTextColor
+    )
+    val semi21 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 21.sp,
+        color = defaultTextColor
+    )
+    val semi22 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        color = defaultTextColor
+    )
+    val semi24 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        color = defaultTextColor
+    )
+
+    val bold15 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp,
+        color = defaultTextColor
+    )
+    val bold18 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        color = defaultTextColor
+    )
+    val bold20 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        color = defaultTextColor
+    )
+}
