d47eba5e2ccf5e10e76e424492c2874af47eedcf
hseun <hseun0732@dms.hs.kr>
2025-06-19 12:26:09 +0900

    delete :: import

--- app/src/main/java/com/hseun/lendy_v2/ui/InputComponent.kt
+++ app/src/main/java/com/hseun/lendy_v2/ui/InputComponent.kt
@@ -1,26 +1,18 @@
 package com.hseun.lendy_v2.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -39,7 +31,6 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hseun.lendy_v2.R
 import com.hseun.lendy_v2.ui.theme.Gray400
@@ -47,7 +38,6 @@ import com.hseun.lendy_v2.ui.theme.Gray500
 import com.hseun.lendy_v2.ui.theme.LendyFontStyle
 import com.hseun.lendy_v2.ui.theme.Main
 import com.hseun.lendy_v2.ui.theme.Red
-import com.hseun.lendy_v2.ui.theme.White
 import com.hseun.lendy_v2.ui.utils.noRippleClickable
 import com.hseun.lendy_v2.utils.InputErrorType
 import com.hseun.lendy_v2.utils.inputErrorMessage
