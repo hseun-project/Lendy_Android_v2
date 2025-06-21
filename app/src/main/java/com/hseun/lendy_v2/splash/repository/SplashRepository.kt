daa208491e459e777eca7acc4d9f1ed727dc8abe
hseun <hseun0732@dms.hs.kr>
2025-06-21 23:12:11 +0900

    refactoring :: Impl

--- app/src/main/java/com/hseun/lendy_v2/splash/repository/SplashRepository.kt
+++ app/src/main/java/com/hseun/lendy_v2/splash/repository/SplashRepository.kt
@@ -1,37 +1,5 @@
 package com.hseun.lendy_v2.splash.repository
 
-import com.hseun.lendy_v2.local.TokenPreference
-import com.hseun.lendy_v2.network.AuthApi
-
-class SplashRepository(
-    private val api: AuthApi,
-    private val pref: TokenPreference
-) {
-    private fun getRefreshToken(): String? = pref.getRefreshToken()
-    private fun saveToken(accessToken: String, refreshToken: String) {
-        pref.setAccessToken("Bearer $accessToken")
-        pref.setRefreshToken("Bearer $refreshToken")
-    }
-    private fun clearToken() {
-        pref.clearAccessToken()
-        pref.clearRefreshToken()
-    }
-    suspend fun autoLogin(): Boolean {
-        val refreshToken = getRefreshToken() ?: return false
-        return try {
-            val response = api.refresh(refreshToken)
-            if (response.isSuccessful) {
-                response.body()?.let {
-                    saveToken(it.accessToken, it.refreshToken)
-                    true
-                } ?: false
-            } else {
-                clearToken()
-                false
-            }
-        } catch (e: Exception) {
-            clearToken()
-            false
-        }
-    }
+interface SplashRepository {
+    suspend fun autoLogin(): Boolean
 }
--- app/src/main/java/com/hseun/lendy_v2/splash/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/hseun/lendy_v2/splash/viewmodel/SplashViewModel.kt
@@ -5,12 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hseun.lendy_v2.splash.repository.SplashRepository
+import com.hseun.lendy_v2.splash.repository.SplashRepositoryImpl
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class SplashViewModel @Inject constructor(
-    private val repository: SplashRepository
+    private val repository: SplashRepositoryImpl
 ) : ViewModel() {
     var isAutoLoginCheck by mutableStateOf(false)
         private set
