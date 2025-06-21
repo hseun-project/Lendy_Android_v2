0a423d1096b6a3506a846a22d6a124a0b1b1ca1b
hseun <hseun0732@dms.hs.kr>
2025-06-21 23:28:28 +0900

    chore :: animation

--- app/src/main/java/com/hseun/lendy_v2/splash/SplashScreen.kt
+++ app/src/main/java/com/hseun/lendy_v2/splash/SplashScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -69,7 +71,9 @@ fun SplashScreen(
                     bottom = 40.dp
                 )
                 .fillMaxWidth(0.7f)
-                .wrapContentHeight(),
+                .wrapContentHeight()
+                .alpha(alpha.value)
+                .scale(scale.value),
             painter = painterResource(id = R.drawable.lendy_logo_full),
             contentDescription = "Lendy Logo",
             contentScale = ContentScale.FillWidth
