be2400fe695d22abad51a5d0350b2f27c012a952
hseun <hseun0732@dms.hs.kr>
2025-06-21 22:46:28 +0900

    add :: Preference

---
+++ app/src/main/java/com/hseun/lendy_v2/local/TokenPreference.kt
@@ -0,0 +1,36 @@
+package com.hseun.lendy_v2.local
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+
+class TokenPreference(context: Context) {
+    private val prefs: SharedPreferences = context.getSharedPreferences("token_prefs", Context.MODE_PRIVATE)
+
+    companion object {
+        private val ACCESS_TOKEN_KEY = "accessToken"
+        private val REFRESH_TOKEN_KEY = "refreshToken"
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun setAccessToken(token: String) {
+        prefs.edit().putString(ACCESS_TOKEN_KEY, token)
+    }
+    fun getAccessToken(): String? {
+        return prefs.getString(ACCESS_TOKEN_KEY, null)
+    }
+    fun clearAccessToken() {
+        prefs.edit().remove(ACCESS_TOKEN_KEY).apply()
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun setRefreshToken(token: String) {
+        prefs.edit().putString(REFRESH_TOKEN_KEY, token)
+    }
+    fun getRefreshToken(): String? {
+        return prefs.getString(REFRESH_TOKEN_KEY, null)
+    }
+    fun clearRefreshToken() {
+        prefs.edit().remove(REFRESH_TOKEN_KEY).apply()
+    }
+}
