78896eaa389a374fbb9580c212bc6ade9767769e
hseun <hseun0732@dms.hs.kr>
2025-06-21 22:46:42 +0900

    add :: Api

---
+++ app/src/main/java/com/hseun/lendy_v2/network/ApiProvider.kt
@@ -0,0 +1,26 @@
+package com.hseun.lendy_v2.network
+
+import com.hseun.lendy_v2.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiProvider {
+    @Singleton
+    private val instance: Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun getInstance(): Retrofit {
+        return instance
+    }
+}
