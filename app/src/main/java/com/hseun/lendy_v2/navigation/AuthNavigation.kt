2dfdae4314909aa58e397e18b7b0824abf547b73
hseun <hseun0732@dms.hs.kr>
2025-06-27 11:56:53 +0900

    add :: navigation

--- app/src/main/java/com/hseun/lendy_v2/navigation/AuthNavigation.kt
+++ app/src/main/java/com/hseun/lendy_v2/navigation/AuthNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.hseun.lendy_v2.auth.signup.SignUpScreen
 import com.hseun.lendy_v2.auth.splash.SplashScreen
 
 @Composable
@@ -18,6 +19,16 @@ fun AuthNavigation(
             SplashScreen(
                 navToMain = {
                     // MainNavigation 호출
+                },
+                navToLogin = {
+                    // 로그인 페이지로 이동
+                }
+            )
+        }
+        composable(NavigationRoutes.SIGN_UP) {
+            SignUpScreen(
+                navToIdentification = {
+                    // 본인인증 페이지로 이동
                 },
                 navToLogin = {
                     // 로그인 페이지로 이동
