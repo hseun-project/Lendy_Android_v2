b99967eee4b434e25e10ef0fa2eb1c08088ab686
hseun <hseun0732@dms.hs.kr>
2025-06-29 17:01:37 +0900

    add :: api

--- app/src/main/java/com/hseun/lendy_v2/network/AuthApi.kt
+++ app/src/main/java/com/hseun/lendy_v2/network/AuthApi.kt
@@ -1,10 +1,12 @@
 package com.hseun.lendy_v2.network
 
+import com.hseun.lendy_v2.network.model.auth.IdentificationResponse
 import com.hseun.lendy_v2.network.model.auth.SendMailRequest
 import com.hseun.lendy_v2.network.model.auth.SignUpRequest
 import com.hseun.lendy_v2.network.model.auth.TokenResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -19,4 +21,7 @@ interface AuthApi {
 
     @POST("$AUTH/signup")
     suspend fun signUp(@Body request: SignUpRequest): Response<TokenResponse>
+
+    @GET("$AUTH/identification")
+    suspend fun getIdentificationUrl(@Header("Authorization") token: String): Response<IdentificationResponse>
 }
