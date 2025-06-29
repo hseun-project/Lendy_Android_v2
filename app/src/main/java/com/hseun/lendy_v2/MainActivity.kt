df53c5b2db42fab3a38ef493741d3c86954cd4db
hseun <hseun0732@dms.hs.kr>
2025-06-29 22:38:32 +0900

    add :: BottomNavigation

--- app/src/main/java/com/hseun/lendy_v2/MainActivity.kt
+++ app/src/main/java/com/hseun/lendy_v2/MainActivity.kt
@@ -8,9 +8,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.hseun.lendy_v2.navigation.BottomNavigation
 import com.hseun.lendy_v2.navigation.LendyNavigation
+import com.hseun.lendy_v2.navigation.NavigationRoutes
 import com.hseun.lendy_v2.ui.LendyTopBar
 import com.hseun.lendy_v2.ui.theme.Lendy_v2Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,10 +36,23 @@ class MainActivity : ComponentActivity() {
 fun LendyScreen(
     modifier: Modifier = Modifier
 ) {
+    val bottomBarRoutes = listOf(
+        NavigationRoutes.HOME,
+        NavigationRoutes.OPEN_LOAN,
+        NavigationRoutes.MY_PAGE
+    )
+
     val navController = rememberNavController()
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStackEntry?.destination?.route
+
     Scaffold (
         topBar = { LendyTopBar() },
-        bottomBar = {}
+        bottomBar = {
+            if (currentDestination in bottomBarRoutes) {
+                BottomNavigation(navController = navController)
+            }
+        }
     ) {
         Box(modifier = modifier.padding(it)) {
             LendyNavigation(
---
+++ app/src/main/java/com/hseun/lendy_v2/navigation/BottomNavigation.kt
@@ -0,0 +1,99 @@
+package com.hseun.lendy_v2.navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.hseun.lendy_v2.R
+import com.hseun.lendy_v2.ui.theme.Main
+import com.hseun.lendy_v2.ui.theme.White
+import com.hseun.lendy_v2.ui.utils.noRippleClickable
+
+sealed class BottomNavItem(
+    val title: Int,
+    val unselectedIcon: Int,
+    val selectedIcon: Int,
+    val screenRoute: String
+) {
+    data object OpenLoan : BottomNavItem(
+        R.string.open_loan,
+        R.drawable.nav_open_loan_unselected,
+        R.drawable.nav_open_loan_selected,
+        NavigationRoutes.OPEN_LOAN
+    )
+
+    data object Home : BottomNavItem(
+        R.string.home,
+        R.drawable.nav_home_unselected,
+        R.drawable.nav_home_selected,
+        NavigationRoutes.HOME
+    )
+
+    data object MyPage : BottomNavItem(
+        R.string.my_page,
+        R.drawable.nav_my_unselected,
+        R.drawable.nav_my_selected,
+        NavigationRoutes.MY_PAGE
+    )
+}
+
+@Composable
+fun BottomNavigation(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    val navItems = listOf<BottomNavItem>(
+        BottomNavItem.OpenLoan,
+        BottomNavItem.Home,
+        BottomNavItem.MyPage
+    )
+    Row(
+        modifier = modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .background(Main)
+            .padding(
+                start = 60.dp,
+                end = 60.dp,
+                top = 12.dp,
+                bottom = 12.dp
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        navItems.forEach { navItem ->
+            Icon(
+                modifier = modifier
+                    .size(40.dp)
+                    .noRippleClickable {
+                        navController.navigate(navItem.screenRoute) {
+                            navController.graph.startDestinationRoute?.let {
+                                popUpTo(it) { saveState = true }
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                painter = painterResource(id = if (navItem.screenRoute == currentRoute) navItem.selectedIcon else navItem.unselectedIcon),
+                contentDescription = stringResource(id = navItem.title),
+                tint = White
+            )
+        }
+    }
+}
