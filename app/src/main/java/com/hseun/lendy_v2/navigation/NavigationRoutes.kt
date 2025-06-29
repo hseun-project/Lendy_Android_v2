4a80f5dce9c7f7a6f7b3945f8aa0ffcbfe7b1286
hseun <hseun0732@dms.hs.kr>
2025-06-29 17:11:34 +0900

    add :: navigation

--- app/src/main/java/com/hseun/lendy_v2/navigation/NavigationRoutes.kt
+++ app/src/main/java/com/hseun/lendy_v2/navigation/NavigationRoutes.kt
@@ -3,4 +3,5 @@ package com.hseun.lendy_v2.navigation
 object NavigationRoutes {
     const val SPLASH = "splash"
     const val SIGN_UP = "signUp"
+    const val IDENTIFICATION = "identification"
 }
