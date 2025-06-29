afd7abbafbf2cf0c458d3387b91915e7b49abdff
hseun <hseun0732@dms.hs.kr>
2025-06-29 16:22:25 +0900

    add :: RepositoryProvider

--- app/src/main/java/com/hseun/lendy_v2/auth/splash/repository/SplashRepository.kt
+++ app/src/main/java/com/hseun/lendy_v2/auth/splash/repository/SplashRepository.kt
@@ -1,5 +1,19 @@
 package com.hseun.lendy_v2.auth.splash.repository
 
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
 interface SplashRepository {
     suspend fun autoLogin(): Boolean
+}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class SplashModule {
+    @Binds
+    abstract fun bindSplashRepository(
+        impl: SplashRepositoryImpl
+    ): SplashRepository
 }
