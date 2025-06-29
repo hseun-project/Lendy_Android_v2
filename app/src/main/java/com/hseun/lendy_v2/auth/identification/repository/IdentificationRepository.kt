99941aa19285f0ea3f250893c4a05d80b4dcbbbc
hseun <hseun0732@dms.hs.kr>
2025-06-29 17:01:48 +0900

    feature :: Identification

---
+++ app/src/main/java/com/hseun/lendy_v2/auth/identification/repository/IdentificationRepository.kt
@@ -0,0 +1,20 @@
+package com.hseun.lendy_v2.auth.identification.repository
+
+import com.hseun.lendy_v2.network.model.auth.IdentificationResponse
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+interface IdentificationRepository {
+    suspend fun getUrl(): Result<IdentificationResponse>
+}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class IdentificationModule {
+    @Binds
+    abstract fun bindIdentificationRepository(
+        impl: IdentificationRepositoryImpl
+    ): IdentificationRepository
+}
