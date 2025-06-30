c83b1be8cf84e42aa4c3a24d4aaef2dc02beab05
hseun <hseun0732@dms.hs.kr>
2025-06-30 11:11:35 +0900

    delete :: import

--- app/src/main/java/com/hseun/lendy_v2/navigation/MainNavigation.kt
+++ app/src/main/java/com/hseun/lendy_v2/navigation/MainNavigation.kt
@@ -1,7 +1,6 @@
 package com.hseun.lendy_v2.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
