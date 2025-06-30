f483187b550939bbf82478f5b08a53624d6440e9
hseun <hseun0732@dms.hs.kr>
2025-06-30 11:57:44 +0900

    add :: api

--- app/src/main/java/com/hseun/lendy_v2/network/UserApi.kt
+++ app/src/main/java/com/hseun/lendy_v2/network/UserApi.kt
@@ -1,5 +1,6 @@
 package com.hseun.lendy_v2.network
 
+import com.hseun.lendy_v2.network.model.user.ApplyListItemData
 import com.hseun.lendy_v2.network.model.user.UserInfoResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -10,4 +11,7 @@ private const val USER = "user"
 interface UserApi {
     @GET("$USER/info")
     suspend fun getUserInfo(@Header("Authorization") token: String): Response<UserInfoResponse>
+
+    @GET("$USER/apply")
+    suspend fun getApplyList(@Header("Authorization") token: String): Response<List<ApplyListItemData>>
 }
