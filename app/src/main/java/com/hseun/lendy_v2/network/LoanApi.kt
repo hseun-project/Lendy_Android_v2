401aa7ca088cbd75ba9cca1a1ed6192a012c839f
hseun <hseun0732@dms.hs.kr>
2025-07-03 23:10:45 +0900

    add :: api

--- app/src/main/java/com/hseun/lendy_v2/network/LoanApi.kt
+++ app/src/main/java/com/hseun/lendy_v2/network/LoanApi.kt
@@ -1,5 +1,7 @@
 package com.hseun.lendy_v2.network
 
+import com.hseun.lendy_v2.network.model.loan.ApplyLoanRequest
+import com.hseun.lendy_v2.network.model.loan.BondListItemData
 import com.hseun.lendy_v2.network.model.loan.ChangeStateRequest
 import com.hseun.lendy_v2.network.model.loan.LentListItemData
 import com.hseun.lendy_v2.network.model.loan.RequestDetailResponse
@@ -10,6 +12,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.PATCH
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -37,4 +40,16 @@ interface LoanApi {
         @Path("applyLoanId") id: Long,
         @Body request: ChangeStateRequest
     ): Response<Unit>
+
+    @POST(LOAN)
+    suspend fun applyLoan(
+        @Header("Authorization") token: String,
+        @Body request: ApplyLoanRequest
+    ): Response<Unit>
+
+    @GET("$LOAN/bond")
+    suspend fun getBondList(
+        @Header("Authorization") token: String,
+        @Query("keyword") keyword: String
+    ): Response<List<BondListItemData>>
 }
---
+++ app/src/main/java/com/hseun/lendy_v2/network/model/loan/BondListItemData.kt
@@ -0,0 +1,7 @@
+package com.hseun.lendy_v2.network.model.loan
+
+data class BondListItemData(
+    val id: Long,
+    val name: String,
+    val email: String
+)
