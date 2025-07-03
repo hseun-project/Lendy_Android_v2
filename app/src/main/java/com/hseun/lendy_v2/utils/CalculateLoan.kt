b40d95ad371d64626007ce630eb8f9a8cde06b6c
hseun <hseun0732@dms.hs.kr>
2025-07-03 15:38:23 +0900

    chore :: return type

--- app/src/main/java/com/hseun/lendy_v2/utils/CalculateLoan.kt
+++ app/src/main/java/com/hseun/lendy_v2/utils/CalculateLoan.kt
@@ -8,17 +8,17 @@ fun calculateInterest(
     during: Int,
     money: Int,
     interestStr: String
-): String {
+): Int {
     try {
         val annualRate = interestStr.toFloat() / 100
         val rate = when(duringType) {
             DuringType.DAY -> annualRate / 365
             DuringType.MONTH -> annualRate / 12
         }
-        val interest = floor((money * rate * during))
-        return interest.toString()
+        val interest = floor((money * rate * during)).toInt()
+        return interest
     } catch (e: Exception) {
         Log.e("calculateInterest", e.message.toString())
-        return "0"
+        return 0
     }
 }
